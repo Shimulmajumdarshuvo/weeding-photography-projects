161d9fd0279a592f2ee93363dc9268f679583063
shimul <shimul@gmail.com>
2022-04-17 19:49:14 +0600

    notfound page updates

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
         <div>
             <div className='not-found'>
                 <h1>404 , Not Found.</h1>
-                <h3>Search for Error 404 not found at MySearchExperts. Check out results for Error 404 not found.</h3>
+                <h3>Search for Error 404 not found at MySearchExperts. <br />
+                    Check out results for Error 404 not found.</h3>
                 <img src={`https://previews.123rf.com/images/kaymosk/kaymosk1804/kaymosk180400005/99776312-erreur-404-page-non-trouv%C3%A9e-erreur-avec-effet-glitch-%C3%A0-l-%C3%A9cran-vector-illustration-pour-votre-concep.jpg`} alt="" />
 
             </div>
