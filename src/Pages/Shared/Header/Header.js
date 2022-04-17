e2b0cc70c8ca0dab9662caa9f4b140fed9b343de
shimul <shimul@gmail.com>
2022-04-17 12:56:59 +0600

    react router hook used Google and Github

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,17 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import './Header.css'
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    }
     return (
         <div>
             <Navbar className='navbar-part' collapseOnSelect expand="lg" sticky='top' variant="dark">
@@ -25,7 +33,14 @@ const Header = () => {
                         <Nav>
                             <Nav.Link as={Link} to="about">About Me</Nav.Link>
                             <Nav.Link as={Link} to="blog">Blog</Nav.Link>
-                            <Nav.Link as={Link} to="login">Login</Nav.Link>
+
+                            {
+                                user ?
+                                    <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>sign out</button>
+                                    :
+                                    <Nav.Link as={Link} to="login">
+                                        Login
+                                    </Nav.Link>}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
