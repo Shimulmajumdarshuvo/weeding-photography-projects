956f0deb4f310c41bea4902bb395e7c75ff1ae2c
shimul <shimul@gmail.com>
2022-04-17 11:45:15 +0600

    checkout proceted route

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import About from './Pages/About/About';
 import Blog from './Pages/Blog/Blog';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
+import RequireAuth from './Pages/Home/RequireAuth/RequireAuth';
+import Checkout from './Pages/Checkout/Checkout';
 
 
 function App() {
@@ -23,6 +25,12 @@ function App() {
         <Route path='/blog' element={<Blog></Blog>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
+
+        <Route path='/checkout' element={
+          <RequireAuth>
+            <Checkout></Checkout>
+          </RequireAuth>
+        }></Route>
 
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
